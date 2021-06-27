dad95c029c8987ca5230fdcaea08591a7923b5d9
tuhm1 <hmtu117@gmail.com>
2021-06-28 04:56:27 +0700

    Delete notifications on delete comment

--- src/db/comment.js
+++ src/db/comment.js
@@ -13,8 +13,16 @@ schema.index({ replyTo: 1, postId: 1, createdAt: 1 });
 
 schema.pre('remove', async function (next) {
     try {
-        const comments = await mongoose.models.comment.find({ replyTo: this._id });
-        await Promise.all(comments.map(c => c.remove()));
+        const p1 = mongoose.models.comment.find({ replyTo: this._id })
+            .then(comments =>
+                Promise.all(comments.map(c => c.remove()))
+            );
+        const p2 = mongoose.models.notification.find({
+            $or: [{ replyId: this._id }, { commentId: this._id }]
+        }).then(notifications =>
+            Promise.all(notifications.map(n => n.remove()))
+        );
+        await Promise.all([p1, p2]);
         next();
     } catch (error) {
         next(error);
