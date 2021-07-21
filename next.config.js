ef44fd1fbffba4f39e0cd72f4f9c1773586e7e16
tuhm1 <hmtu117@gmail.com>
2021-07-21 14:20:18 +0700

    Fix build error

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
     },
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            config.node = {
+                fs: 'empty'
+            }
+        }
+        return config
+    }
 };
